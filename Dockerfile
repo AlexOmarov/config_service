028b3c32e55ff0cf782b9fc96205bab347ecc6fb
Alex Omarov <decentboat@gmail.com>
2021-02-25 13:22:02 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM openjdk:14-jdk-alpine
+
+LABEL maintainer="dungeonswdragons@gmail.com"
+
+RUN apk add openssh
+RUN addgroup -S dwd && adduser -S dwd -G dwd
+
+RUN mkdir -p /home/dwd/.ssh
+RUN chmod 777 /home/dwd/.ssh
+#Add id_rsa and known hosts files
+COPY id_rsa /home/dwd/.ssh/id_rsa
+
+COPY known_hosts /home/dwd/.ssh/known_hosts
+
+USER dwd:dwd
+VOLUME /tmp
+EXPOSE 8080
+
+ARG DEPENDENCY=build/deps
+COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY ${DEPENDENCY}/META-INF /app/META-INF
+COPY ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENTRYPOINT ["java","-cp","app:app/lib/*","ru/somarov/config_service/ConfigServiceApplicationKt"]
---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	kotlin("jvm") version "1.4.0"
+	kotlin("plugin.spring") version "1.4.0"
+}
+
+group = "ru.somarov"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springCloudVersion"] = "2020.0.0-SNAPSHOT"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+/* Custom image layering implementation. May be omitted due to existence of https://spring.io/blog/2020/01/27/creating-docker-images-with-spring-boot-2-3-0-m1 */
+gradle.buildFinished {
+	val srcDir = File("$buildDir/libs/${rootProject.name}-${version}.jar")
+	if (srcDir.exists()) {
+		println("Extracting deps")
+		delete("build/deps")
+		mkdir("build/deps")
+		org.gradle.kotlin.dsl.support.unzipTo(file("build/deps"), srcDir)
+	} else {
+		println("No jar found, please build the project first")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
